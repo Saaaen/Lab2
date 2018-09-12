a0edacca92ccb96c2df74546ae349e0642745630
Saen <saenc2@illinois.edu>
2018-09-12 16:22:10 -0500

    write the code

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,32 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            if (maze.isFinished()) {
+                break;
+            } else {
+                maze.turnLeft();
+                if (maze.canMove()){
+                    maze.move();
+                    continue;
+                } else {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                        continue;
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove()) {
+                            maze.move();
+                            continue;
+                        } else {
+                            maze.turnRight();
+                            maze.move();
+                            continue;
+                        }
+                    }
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
